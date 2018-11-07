8a13d8310b900b324d2c8d9f25a9094e7d1f9899
mapachurro <oliver.renwick@gmail.com>
2018-11-06 20:33:26 -0500

    tuesday updates

---
+++ hard loop animals.js
@@ -0,0 +1,13 @@
+<html>
+<script>
+var myFarm = ["chickens", "pigs", "cows", "horses", "ostriches"];
+
+for (var a = 0; a < myFarm.length; a++) {
+    console.log(myFarm[i]);
+}
+
+if (myfarm[i].charAt(0) === "c" || myFarm[i].charAt(0) === "o") {
+    alert("starts with a c or an o!")
+}
+</script>
+</html>
